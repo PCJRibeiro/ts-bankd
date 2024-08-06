b4ecf88d3fbb1bba19faf7a0fa4b3acbf6404d91
PCJRibeiro <pcjr2003@gmail.com>
2024-08-06 17:04:19 -0300

    codigo finalizado

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,29 @@
-interface ICard {
-  id: number,
-  paragraph: string,
-  details: string
-}
+import { Box, Button, Center, ChakraProvider, Input } from "@chakra-ui/react"
+import { login } from "../services/login"
+import { Layout } from "./Layout"
 
-export const Card = ({ id, paragraph, details }: ICard) => {
-  return(
-    <div>
-      <h1>Card {id}</h1>
-      <p>{paragraph}</p>
-      <p>{details}</p>
-    </div>
+
+export const Card = () => {
+  return (
+    <Layout>
+      <ChakraProvider>
+        <Box minHeight='100vh' backgroundColor='#9413dc' padding='25px'>
+          <Center>
+            <Box backgroundColor='#000' borderRadius='25px' padding='15px' >
+              <Center color='#fff'>
+                <h1>Faça o login</h1>
+              </Center>
+              <Input placeholder="email" backgroundColor='#ffff' marginBottom='3px' />
+              <Input placeholder="password" backgroundColor='#ffff' />
+              <Center>
+                <Button onClick={login} colorScheme='purple' size='sm' width='100%' marginTop='5px'>
+                  Entrar
+                </Button>
+              </Center>
+            </Box>
+          </Center>
+        </Box>
+      </ChakraProvider>
+    </Layout>
   )
 }
